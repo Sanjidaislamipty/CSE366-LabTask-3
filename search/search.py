a7ab81421f558492efdfc68ec746fb3e973c7c6e
Sanjidaislamipty <149031459+Sanjidaislamipty@users.noreply.github.com>
2024-11-30 23:48:53 +0600

    Update search.py

--- search/search.py
+++ search/search.py
@@ -187,7 +187,7 @@ def aStarSearch(problem, heuristic=nullHeuristic):
     return []
 
 
-# Abbreviations
+# Abbreviations 
 bfs = breadthFirstSearch
 dfs = depthFirstSearch
 ucs = uniformCostSearch
